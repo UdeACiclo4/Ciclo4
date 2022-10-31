00063896ebc580ce3cbb23bc201a077772457fd0
Yohan-Rodriguez <ymrodriguezg@upn.edu.co>
2022-10-30 21:40:47 -0500

    Templete Administrador vista Productos creados

---
+++ Desktop/Error404/react1/src/components/AppAdminVerProduc.js
@@ -0,0 +1,22 @@
+import './styless/AppAdminVerProduc.css';
+
+function AppAdminSeeProduct(){
+    return(
+        <div>
+            <header Class="HeaderAdmin">
+                <nav> 
+                    <ul>
+                        <h Class="Text1">Lista productos</h>       
+                        <h Class="Text1"> Modificar productos</h>  
+                        <h Class="Text1"> Lista ventas</h>
+                    </ul>
+                </nav>
+            </header>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default AppAdminSeeProduct;
