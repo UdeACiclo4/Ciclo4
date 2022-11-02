90fd175ac5b7044f950f182a3cc27b435970c0a0
Cristian Lopez <cristian__host@hotmail.com>
2022-11-01 22:44:52 -0500

    update

---
+++ Downloads/Proyecto/src/data.js
@@ -0,0 +1,44 @@
+export const stockData = [
+    {
+      nombre: "Twitter Inc",
+      ticker: "TWTR",
+      precio: "22.76 USD",
+      stock: "5",
+      timeElapsed: "5 sec ago",
+    },
+    {
+      nombre: "Square Inc",
+      ticker: "SQ",
+      precio: "45.28 USD",
+      stock: "8",
+      timeElapsed: "10 sec ago",
+    },
+    {
+      nombre: "Shopify Inc",
+      ticker: "SHOP",
+      precio: "341.79 USD",
+      stock: "15",
+      timeElapsed: "3 sec ago",
+    },
+    {
+      nombre: "Sunrun Inc",
+      ticker: "RUN",
+      precio: "9.87 USD",
+      stock: "3",
+      timeElapsed: "4 sec ago",
+    },
+    {
+      nombre: "Adobe Inc",
+      ticker: "ADBE",
+      precio: "300.99 USD",
+      stock: "12",
+      timeElapsed: "10 sec ago",
+    },
+    {
+      nombre: "HubSpot Inc",
+      ticker: "HUBS",
+      precio: "115.22 USD",
+      stock: "1",
+      timeElapsed: "12 sec ago",
+    },
+  ];
